eb6c154367b162eb7623a3d2268d13ea808f2a8a
agha-seyed <agha.seyed.ehsan@gmail.com>
2025-07-30 10:18:26 +0200

    Create __init__.py

---
+++ studentbot/utils/__init__.py
@@ -0,0 +1,11 @@
+# studentbot/utils/__init__.py
+from .ai_utils import smart_search
+from .alert_admin import notify_admin_unanswered
+from .gpt_utils import ask_gpt
+from .rss_utils import fetch_rss_articles
+from .text_formatter import get_translated_text, sanitize_markdown, search_in_documents
+from .weekly_report import start_weekly_scheduler
+from .gsheets import gsheets_client
+from .db_utils import AsyncSessionLocal, create_users_table, create_consultation_requests_table
+from .redis_utils import get_cached_answer, cache_answer
+from .scheduler import start_scheduler
