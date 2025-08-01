b502228bc0e4857afb0a304eec9da91f3b5d7c7a
agha-seyed <agha.seyed.ehsan@gmail.com>
2025-08-01 19:24:21 +0200

    Update profile_handler.py

--- studentbot/handlers/profile_handler.py
+++ studentbot/handlers/profile_handler.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, CommandHandler, CallbackQueryHandler
 from telegram.error import TelegramError
-from studentbot.utils.text_formatter import get_translated_text, sanitize_markdown
+from studentbot.utils.common import get_translated_text, sanitize_markdown
 from studentbot.utils.db_utils import (
     get_user,
     delete_user,
