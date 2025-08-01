175c7690cfc5852f64116bfdb6d16334163b86f8
agha-seyed <agha.seyed.ehsan@gmail.com>
2025-08-01 12:51:43 +0200

    Update __init__.py

--- studentbot/utils/__init__.py
+++ studentbot/utils/__init__.py
@@ -1,7 +1,8 @@
 # studentbot/utils/__init__.py
+from .common import get_translated_text, sanitize_markdown
 from .alert_admin import notify_admin_unanswered
 from .rss_utils import fetch_rss_articles
-from .text_formatter import get_translated_text, sanitize_markdown
+from .text_formatter import send_email
 from .text_extractor import search_in_documents
 from .scheduler import scheduler
 from .gsheets import gsheets_client
