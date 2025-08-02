5d661fb770ad2110fe0f6ec64e6eec36b6f4047a
agha-seyed <agha.seyed.ehsan@gmail.com>
2025-08-02 18:02:01 +0200

    Update weather_handler.py

--- studentbot/handlers/weather_handler.py
+++ studentbot/handlers/weather_handler.py
@@ -4,7 +4,7 @@ import httpx
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, CommandHandler, CallbackQueryHandler
 from telegram.error import TelegramError
-from studentbot.utils.common import get_translated_text, sanitize_markdown
+from studentbot.utils.common import get_translated_text, sanitize_markdown  # Changed from text_formatter
 from studentbot.utils.db_utils import get_user, AsyncSessionLocal, log_event
 from studentbot.utils.gsheets import gsheets_client
 from studentbot.handlers.gamification_handler import award_points_for_action
