3dce85ab2109f88640b211a7ae1139ad443afeb4
agha-seyed <agha.seyed.ehsan@gmail.com>
2025-08-02 17:44:58 +0200

    Update migration_handler.py

--- studentbot/handlers/migration_handler.py
+++ studentbot/handlers/migration_handler.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler
 from telegram.error import TelegramError
 from sqlalchemy import select, update
-from studentbot.utils.text_formatter import get_translated_text, sanitize_markdown
+from studentbot.utils.common import get_translated_text, sanitize_markdown  # Changed from text_formatter
 from studentbot.utils.db_utils import AsyncSessionLocal, get_user, log_event
 from studentbot.utils.gsheets import gsheets_client
 from studentbot.utils.models_db import MigrationStatus
